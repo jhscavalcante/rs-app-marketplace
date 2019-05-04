f03b873f471d599dbbe5ef574b3b64ed193ffaf4
Henrique Cavalcante <henrique.cavalcante@yahoo.com.br>
2019-05-04 11:44:08 -0400

    Configuração do ORM (Mongoose) com Database MongoDB

--- src/server.js
+++ src/server.js
@@ -1,12 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const databaseConfig = require('./config/database')
 
 class App {
   constructor () {
     this.express = express()
     this.isDev = process.env.NODE_ENV !== 'production'
 
+    this.database()
     this.middlewares()
     this.routes()
+  }
+
+  database () {
+    // as configurações useCreateIndex e useNewUrlParser é para que o mongoose
+    // saiba que está sendo usado a versão mais recente do node,
+    // portanto o mongoose precisará fazer algumas adaptações
+    mongoose.connect(databaseConfig.uri, {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    })
   }
 
   middlewares () {
