d4ad3aff88d85ffe47d1c72664327e9c7fe4d40f
Henrique Cavalcante <henrique.cavalcante@yahoo.com.br>
2019-05-06 22:27:41 -0400

    Solicitação de compra - uso da lib (nodemailer)

---
+++ src/app/controllers/PurchaseController.js
@@ -0,0 +1,26 @@
+const Ad = require('../models/Ad')
+const User = require('../models/User')
+const Mail = require('../services/Mail')
+
+class PurchaseController {
+  async store (req, res) {
+    const { ad, content } = req.body
+
+    // busca o anúncio e as informações do anúncio e do author do anúncio
+    const purchaseAd = await Ad.findById(ad).populate('author')
+
+    // busca as informações do usuário logado
+    const user = await User.findById(req.userId)
+
+    await Mail.sendMail({
+      from: '',
+      to: purchaseAd.author.email,
+      subject: `Solicitação de compra: ${purchaseAd.title}`,
+      html: `<p>Teste: ${content} </p>`
+    })
+
+    return res.send()
+  }
+}
+
+module.exports = new PurchaseController()
---
+++ src/config/mail.js
@@ -0,0 +1,9 @@
+module.exports = {
+  host: '',
+  port: ,
+  secure: ,
+  auth: {
+    user: '',
+    pass: ''
+  }
+}
